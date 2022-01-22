992c70469fcecf0ab96c9940efead9a9961bfee4
JuanWigg <jgwiggenhauser@gmail.com>
2022-01-22 00:35:23 -0300

    Adds Hello World App

---
+++ App/src/main/java/com/app/hello/HelloApplication.java
@@ -0,0 +1,22 @@
+package com.app.hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@SpringBootApplication
+@RestController
+public class HelloApplication {
+
+	@GetMapping("/")
+	public String index() {
+		return "Hello World!";
+	}
+	
+	public static void main(String[] args) {
+		SpringApplication.run(HelloApplication.class, args);
+	}
+
+}
